d4602271d8f6d6a3795312f4b10f28bf4e7cea48
ivan8557 <ishyrba@gmail.com>
2026-07-21 13:14:07 +0300

    Fix Render port

--- server.py
+++ server.py
@@ -113,5 +113,10 @@ def order():
     response = jsonify({'ok':True})
     return add_cors_headers(response), 200
 
+import os
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8001)
+    app.run(
+        host='0.0.0.0',
+        port=int(os.environ.get("PORT", 8001))
+    )
